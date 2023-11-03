7bf18c7074de6e4ba8370e67ef70bf2291eb37a0
Gubio Garcia <gubiogarcia@gmail.com>
2023-11-02 23:04:59 -0300

    Resolucao Desafio

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TrilhaApiDesafio.Models
+namespace AgendamentoDeTarefas.Models
 {
     public class Tarefa
     {
