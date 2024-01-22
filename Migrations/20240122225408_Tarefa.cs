38608e310c2568478159189bf803338b20b764fd
Igor Carvalho <ig.carvalho001@gmail.com>
2024-01-22 19:58:17 -0300

    Corrigindo as migrations

---
+++ Migrations/20240122225408_Tarefa.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrilhaApiDesafio.Migrations
+{
+    public partial class Tarefa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
